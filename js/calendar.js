cd9405a1b9ec9fa22dc56d715506814ee1507037
Unknown <luizhrios@gmail.com>
2018-08-23 20:21:33 -0300

    imgs + calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,143 +1,155 @@
-$(function() {
-	  $('#calendar').fullCalendar({
-		locale: 'pt-BR',
-		events: [
+$(function ()
+{
+	  $('#calendar').fullCalendar(
+    {
+		header:
+            {
+			left:'prev,next today', 
+			center:'title', 
+			right:'month,listMonth'
+		  }, 
+		eventClick:function(calEvent, jsEvent, view)
+        {
+			alert('Event: ' + calEvent.title); 	
+		}, 
+		locale:'pt-BR', 
+		events:[
 		{
-			title  : 'Ajuste dos mapas conceituais e finalizar o site',
-			start  : '2018-08-23'
-		},
+			title:'Ajuste dos mapas conceituais e finalizar o site', 
+			start:'2018-08-23'
+		}, 
 		{
-			title  : 'Entregas da semana em sala',
-			start  : '2018-08-24'
-		},
+			title:'Entregas da semana em sala', 
+			start:'2018-08-24'
+		}, 
 		{
-			title  : 'Reunião geral por video conferencia 21:40',
-			start  : '2018-08-27'
-		},
+			title:'Reunião geral por video conferência', 
+			start:'2018-08-27T21:40'
+		}, 
 		{
-			title  : 'Reunião de cada grupo dividido pela gestão ',
-			start  : '2018-08-28'
-		},
+			title:'Reunião de cada grupo dividido pela gestão ', 
+			start:'2018-08-28'
+		}, 
 		{
-			title  : 'Análise da documentação do semestre anterior',
-			start  : '2018-08-29'
-		},
+			title:'Análise da documentação do semestre anterior', 
+			start:'2018-08-29'
+		}, 
 		{
-			title  : 'Entregas da semana em sala',
-			start  : '2018-08-31'
-		},
+			title:'Entregas da semana em sala', 
+			start:'2018-08-31'
+		}, 
 		{
-			title  : 'Reunião geral por video conferencia 21:40',
-			start  : '2018-09-03'
-		},
+			title:'Reunião geral por video conferência', 
+			start:'2018-09-03T21:40'
+		}, 
 		{
-			title  : 'Reunião de cada grupo dividido pela gestão',
-			start  : '2018-09-04'
-		},
+			title:'Reunião de cada grupo dividido pela gestão', 
+			start:'2018-09-04'
+		}, 
 		{
-			title  : 'Feriado Nacional',
-			start  : '2018-09-07',
-			backgroundColor : '#ffff00',
-			textColor : '#000'
-		},
+			title:'Feriado Nacional', 
+			start:'2018-09-07', 
+			backgroundColor:'#ffff00', 
+			textColor:'#000'
+		}, 
 		{
-			title  : 'Reunião geral por video conferencia 21:40',
-			start  : '2018-09-10'
-		},
+			title:'Reunião geral por video conferência', 
+			start:'2018-09-10T21:40'
+		}, 
 		{
-			title  : 'Reunião de cada grupo dividido pela gestão',
-			start  : '2018-09-11'
-		},
+			title:'Reunião de cada grupo dividido pela gestão', 
+			start:'2018-09-11'
+		}, 
 		{
-			title  : 'Entregas da semana em sala',
-			start  : '2018-09-14'
-		},
+			title:'Entregas da semana em sala', 
+			start:'2018-09-14'
+		}, 
 		{
-			title  : 'Reunião geral por video conferencia 21:40',
-			start  : '2018-09-17'
-		},
+			title:'Reunião geral por video conferência', 
+			start:'2018-09-17T21:40'
+		}, 
 		{
-			title  : 'Reunião de cada grupo dividido pela gestão',
-			start  : '2018-09-18'
-		},
+			title:'Reunião de cada grupo dividido pela gestão', 
+			start:'2018-09-18'
+		}, 
 		{
-			title  : 'Entregas da semana em sala',
-			start  : '2018-09-21'
-		},
+			title:'Entregas da semana em sala', 
+			start:'2018-09-21'
+		}, 
 		{
-			title  : 'Reunião geral por video conferencia 21:40',
-			start  : '2018-09-24'
-		},
+			title:'Reunião geral por video conferência', 
+			start:'2018-09-24T21:40'
+		}, 
 		{
-			title  : 'Reunião de cada grupo dividido pela gestão',
-			start  : '2018-09-25'
-		},
+			title:'Reunião de cada grupo dividido pela gestão', 
+			start:'2018-09-25'
+		}, 
 		{
-			title  : 'Entregas da semana em sala',
-			start  : '2018-09-28'
-		},
+			title:'Entregas da semana em sala', 
+			start:'2018-09-28'
+		}, 
 		{
-			title  : 'Reunião geral por video conferencia 21:40',
-			start  : '2018-10-01'
-		},
+			title:'Reunião geral por video conferência', 
+			start:'2018-10-01T21:40'
+		}, 
 		{
-			title  : 'Reunião de cada grupo dividido pela gestão',
-			start  : '2018-10-02'
-		},
+			title:'Reunião de cada grupo dividido pela gestão', 
+			start:'2018-10-02'
+		}, 
 		{
-			title  : 'Entregas da semana em sala',
-			start  : '2018-10-05'
-		},
+			title:'Entregas da semana em sala', 
+			start:'2018-10-05'
+		}, 
 		{
-			title  : 'Reunião geral por video conferencia 21:40',
-			start  : '2018-10-08'
-		},
+			title:'Reunião geral por video conferência', 
+			start:'2018-10-08T21:40'
+		}, 
 		{
-			title  : 'Reunião de cada grupo dividido pela gestão',
-			start  : '2018-10-09'
-		},
+			title:'Reunião de cada grupo dividido pela gestão', 
+			start:'2018-10-09'
+		}, 
 		{
-			title  : 'Feriado Nacional',
-			start  : '2018-10-12',
-			backgroundColor : '#ffff00',
-			textColor : '#000'
-		},
+			title:'Feriado Nacional', 
+			start:'2018-10-12', 
+			backgroundColor:'#ffff00', 
+			textColor:'#000'
+		}, 
 		{
-			title  : 'Reunião geral por video conferencia 21:40',
-			start  : '2018-10-15'
-		},
+			title:'Reunião geral por video conferência', 
+			start:'2018-10-15T21:40'
+		}, 
 		{
-			title  : 'Reunião de cada grupo dividido pela gestão',
-			start  : '2018-10-16'
-		},
+			title:'Reunião de cada grupo dividido pela gestão', 
+			start:'2018-10-16'
+		}, 
 		{
-			title  : 'Entregas da semana em sala',
-			start  : '2018-10-19'
-		},
+			title:'Entregas da semana em sala', 
+			start:'2018-10-19'
+		}, 
 		{
-			title  : 'Reunião geral por video conferencia 21:40',
-			start  : '2018-10-22'
-		},
+			title:'Reunião geral por video conferência', 
+			start:'2018-10-22T21:40'
+		}, 
 		{
-			title  : 'Reunião de cada grupo dividido pela gestão',
-			start  : '2018-10-23'
-		},
+			title:'Reunião de cada grupo dividido pela gestão', 
+			start:'2018-10-23'
+		}, 
 		{
-			title  : 'Entregas da semana em sala',
-			start  : '2018-10-26'
-		},
+			title:'Entregas da semana em sala', 
+			start:'2018-10-26'
+		}, 
 		{
-			title  : 'Entrega parcial dos projetos para equipe',
-			start  : '2018-10-29'
-		},
+			title:'Entrega parcial dos projetos para equipe', 
+			start:'2018-10-29'
+		}, 
 		{
-			title  : 'Reunião de cada grupo dividido pela gestão',
-			start  : '2018-10-30'
-		},
+			title:'Reunião de cada grupo dividido pela gestão', 
+			start:'2018-10-30'
+		}, 
 		{
-			title  : 'Entrega final dos projetos para o professor',
-			start  : '2018-11-02'
+			title:'Entrega final dos projetos para o professor', 
+			start:'2018-11-02'
 		}
 	  ]
 	  })
-	});
+	}); 
